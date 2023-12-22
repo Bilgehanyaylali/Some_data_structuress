cc0d3db6995173e3f8b0e6d7a988a8d0e8bdd6c6
Bilgehanyaylali <bilgehan.yaylali@gmail.com>
2023-12-22 15:20:20 +0300

    Update Tuple.py

--- Tuple.py
+++ Tuple.py
@@ -20,6 +20,14 @@ tuple2[1:]  # ('y', 'z', [1, 2, 3])
 
 tuple1[0] =99  # TypeError: 'tuple' object does not support item assignment
 
+# Some tuple methods #
 
+tuple1.count("a")  # 1
+
+tuple2.count("a")  # 0
+
+tuple1.index(3)  # 2
+
+tuple1.index("a") # 3
 
 
