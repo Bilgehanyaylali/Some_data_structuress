d6c121b8cf7d7186238392068b7b1c792fe4fe1b
Bilgehanyaylali <bilgehan.yaylali@gmail.com>
2023-12-22 15:16:59 +0300

    Create Tuple.py

---
+++ Tuple.py
@@ -0,0 +1,25 @@
+# Tuple Structures #
+
+tuple1 = (1, 2, 3, "a", "b", "c")
+
+tuple2 = ("x","y","z",[1, 2, 3])
+
+# I use index method #
+
+m = tuple1[0]  # m=1
+
+n = tuple2[3][1]  # n=2
+
+print(m**n)  # 1
+
+tuple1[:5]  # (1, 2, 3, 'a', 'b')
+
+tuple2[1:]  # ('y', 'z', [1, 2, 3])
+
+# I change some values #
+
+tuple1[0] =99  # TypeError: 'tuple' object does not support item assignment
+
+
+
+
