5ead193b8c811fa7a01d21f87d0f701624485922
Bilgehanyaylali <bilgehan.yaylali@gmail.com>
2023-12-22 14:50:57 +0300

    Last update

--- Dictionary.py
+++ Dictionary.py
@@ -1,2 +1,49 @@
-print("Ali hoca ile deneme")
-print(5+6)
+# Dictionary Structures #
+dictionary1 = {"a": 1,
+               "b": 2,
+               "c": 3}
+
+dictionary2 = {"a": [1, "x"],
+               "b": [2, "y"],
+               "c": [3, "z"]}
+
+# I find relation between key and value #
+dictionary1["a"]  # 1
+
+dictionary2["b"]  # [2, "y"]
+
+dictionary2["c"][1]  # 'z'
+
+dictionary2.get("c")  # [3, "z"]
+
+# Key inquiry #
+
+"a" in dictionary1  # True
+
+"d" in dictionary1  # False
+
+# I change some values #
+
+dictionary1["a"]= 99  # {'a': 99, 'b': 2, 'c': 3}
+
+dictionary2["c"]= 111  # {'a': [1, 'x'], 'b': [2, 'y'], 'c': 111}
+
+# I find all keys #
+
+dictionary1.keys()  # dict_keys(['a', 'b', 'c'])
+
+# I find all values #
+
+dictionary1.values()  # dict_values([99, 2, 3])
+
+# I find keys and values together#
+
+dictionary1.items()  # dict_items([('a', 99), ('b', 2), ('c', 3)])
+
+# I update key and value #
+
+dictionary1.update({"a": 1})  # {'a': 1, 'b': 2, 'c': 3}
+
+dictionary2.update({"c": [3,"z"], "d": [4, "t"] })  # {'a': [1, 'x'], 'b': [2, 'y'], 'c': [3, 'z'], 'd': [4, 't']}
+
+
--- List.py
+++ List.py
@@ -5,7 +5,7 @@ list2 = ["x", "y", "z"]
 
 list3 = [12, 13, 14, "p", "q", "r", [101, 102, "a", "b", "c"]]
 
-# I will use index method #
+# I use index method #
 
 m = list3[0]  # m = 12
 
@@ -25,7 +25,7 @@ o = list3[6]  # o = [101, 102, "a", "b", "c"]
 
 p = list3[6][2]  # p = 'a'
 
-# I will change some values #
+# I change some values #
 
 list2[1] = 9999  # list2 = ['x', 9999, 'z']
 
